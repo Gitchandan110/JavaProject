799cefa378c3c2c8c0da53c9c0e233b3b7a6cafa
chandan kumar <chandan.kumar@netsutra.com>
2019-09-30 17:40:25 +0530

    pmc

--- Java Project/src/packageJava/JC_36_StringBasic.java
+++ Java Project/src/packageJava/JC_36_StringBasic.java
@@ -5,26 +5,38 @@ public class JC_36_StringBasic {
 	public static void main(String[] args) {
 		
 		String s1=new String("Computer");
+		
 		System.out.println("s1 is :"+s1);
 		
 		
 		String s2="My Computer";
+		
 		System.out.println("s2 is :"+s2);
 		
 		
     	String s3=new String("Laptop");
+    	
     	System.out.println("s3 is :"+s3);
+    	
         String s4=s3.toUpperCase();
+        
 	    System.out.println("s4 is :"+s4);	
+	    
 	    String s5=s3.toLowerCase();
+	    
 	    System.out.println("s5 is :" +s5);
 	    
 	    
 	    String s6="Congratulations";
+	    
 	    System.out.println("s6 is:"+ s6);
+	    
 	    int g = s6.indexOf("g");
+	    
 	    System.out.println("Index of g is:"+ g);
+	    
 	    int a = s6.indexOf("a");
+	    
 	    System.out.println("Index of a is:"+ a);
 	  
 		
---
+++ Java Project/src/packageJava/JC_41_StaticVariable.java
@@ -0,0 +1,49 @@
+package packageJava;
+
+public class JC_41_StaticVariable {
+
+	// Before view this code please view JC_31_Constructor_This.java Program.
+
+	// Instance Variable
+	int EmpId;
+	String EmpName;
+//	String ComanyName;
+	/*
+	 * As company name is fixed for all employee so we can make it Static variable
+	 * as it has common value for all the object
+	 */
+
+	static String CompanyName="Netsutra";
+	static String Address="Ghaziabad";
+
+	// Create Parameterized Constructor (Constructor has same name as class)
+
+	JC_41_StaticVariable(int EmpId, String EmpName) {
+
+		this.EmpId = EmpId;
+		this.EmpName = EmpName;
+		
+	}
+
+	// Created new method "empDetails" inside Class "StaticVariableProgramme"
+
+	public void empDetails() {
+
+		System.out.println(EmpId + " " + EmpName + " "+" "+CompanyName+" "+Address);
+
+	}
+
+	public static void main(String[] args) {
+
+		JC_41_StaticVariable svp = new JC_41_StaticVariable(01, "chandan");
+		svp.empDetails();
+
+		JC_41_StaticVariable svp1 = new JC_41_StaticVariable(02, "Ritesh");
+		svp1.empDetails();
+
+		JC_41_StaticVariable svp2 = new JC_41_StaticVariable(03, "Dev");
+		svp2.empDetails();
+
+	}
+
+}
