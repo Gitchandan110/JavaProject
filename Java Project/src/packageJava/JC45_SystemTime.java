09db41f313e2306ecd679dea46d82bddf43c7fed
chandan kumar <chandan.kumar@netsutra.com>
2019-12-31 17:01:11 +0530

    cu

---
+++ Java Project/src/packageJava/JC45_SystemTime.java
@@ -0,0 +1,19 @@
+package packageJava;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class JC45_SystemTime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Calendar cal = Calendar.getInstance();
+		Date date = cal.getTime();
+		DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+		String formattedDate = dateFormat.format(date);
+		System.out.println("Current time of the day using Calendar - 24 hour format: " + formattedDate);
+	}
+}
